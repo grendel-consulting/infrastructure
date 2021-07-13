ec905c74e86a7ac0aed84a78bb1037e446444930
ramirezj <ramirezj@users.noreply.github.com>
2021-07-13 08:57:37 +0100

    chore: refactor terraform block to versions.tf

--- main.tf
+++ main.tf
@@ -1,27 +1,3 @@
-terraform {
-  required_version = "~> 1.0"
-
-  backend "remote" {
-    organization = "grendel-consulting"
-
-    workspaces {
-      name = "infrastructure"
-    }
-  }
-
-  required_providers {
-    archive = {
-      source  = "hashicorp/archive"
-      version = ">= 2.2.0"
-    }
-
-    aws = {
-      source  = "hashicorp/aws"
-      version = ">= 3.4.0"
-    }
-  }
-}
-
 module "secure_static_site" {
   source = "./modules/secure-static-site"
   providers = {
